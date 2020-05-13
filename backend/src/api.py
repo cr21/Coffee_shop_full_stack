39bd83b14e0657c61e5dde73cfa64d3c4e585719
chirag <cr.tagadiya@gmail.com>
2020-05-13 15:41:03 -0400

    Auth Service Implemented

--- backend/src/api.py
+++ backend/src/api.py
@@ -27,7 +27,10 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route("/drinks")
+@requires_auth
+def get_drinks():
+    
 
 '''
 @TODO implement endpoint
